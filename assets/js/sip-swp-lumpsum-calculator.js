0eeef0904306ec8331a32310bb02aef7ebf96de3
Simply Web <164472125+getsimplyweb@users.noreply.github.com>
2025-05-29 17:54:59 +0530

    c

--- assets/js/sip-swp-lumpsum-calculator.js
+++ assets/js/sip-swp-lumpsum-calculator.js
@@ -31,8 +31,8 @@ function calculateSIP() {
     let n = Number(document.getElementById("sipYears").value) * 12;
     let i = Number(document.getElementById("inflationRate").value) / 100;
 
-    let futureValue = P * ((Math.pow(1 + r, n) - 1) / r) * (1 + r);
-    futureValue /= Math.pow(1 + i, n / 12);
+    let futureValue = P * ((Math.pow(1 + (r - i), n) - 1) / r) * (1 + (r - i));
+    //futureValue /= Math.pow(1 + i, n / 12);
 
     let amountInvested = P * n;
     let profitEarned = futureValue - amountInvested;
